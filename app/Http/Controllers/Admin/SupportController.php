2f769e5df4289571f82e8a8147b82ef8f4b5097b
Marcus Vinicius <marcusferra01@gmail.com>
2024-08-06 14:54:45 -0300

    Three Commit

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support)
     {
-        return view('admin/supports/index');
+        $supports = $support->all();
+        
+        return view('admin/supports/index', compact('supports'));
+    }
+
+    public function create()
+    {
+        return view('admin/supports/create');
+    }
+
+    public function store(Request $request, Support $support)
+    {
+        $data = $request->all();
+        $data['status'] = "a";
+
+        $support->create($data);
+
+        return redirect()->route('supports.index');
     }
 }
